5d4bea7bd958ca020fc5fabc2933cf416bf8419a
Igor <promaster666666@gmail.com>
2020-03-12 10:59:16 +0300

    1

--- rest.groovy
+++ rest.groovy
@@ -1,3 +1,25 @@
-def hi() {
-    println("Hello World!")
+def workspaceCleaning() {
+	docker.image("${nexusRegistry}:5000/centos:8").inside('-u root') {
+		dir("${workspace}") {
+    		sh "chown -R 997 *"
+		}
+	}
+	deleteDir()
+		dir("${workspace}@tmp") {
+    		deleteDir()
+		}
 }
+
+def gitClone(gitUID, gitURL, scmBranch, robotCredentialsId) {
+	def useBranch = scmBranch ? scmBranch : "master"
+	dir("${gitUID}") {
+		git(
+			url: "${gitURL}",
+			branch: "${useBranch}",
+			credentialsId: "${robotCredentialsId}"
+		)
+
+	}
+}
+
+return this
